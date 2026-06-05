37bbccf556a88837c50c1585751cd691fdd9d1f7
Katherine Chen <katisabela125@gmail.com>
2026-06-04 23:01:37 -0400

    Handle sign offset

--- BLIT.cpp
+++ BLIT.cpp
@@ -30,8 +30,8 @@
 #define Sign_TOP    177
 #define Sign_RIGHT  225
 #define Sign_BOTTOM 192
-#define Sign_width  (Sign_RIGHT  - Sign_LEFT)*2
-#define Sign_height (Sign_BOTTOM - Sign_TOP)*2
+#define Sign_width  (Sign_RIGHT  - Sign_LEFT)
+#define Sign_height (Sign_BOTTOM - Sign_TOP)
 #define House_LEFT   127
 #define House_TOP    31
 #define House_RIGHT  191
@@ -101,7 +101,7 @@ int main()
 		int dy_sign = (int)new_y - (int)(450);
 		if (dx_sign < 0) dx_sign = -dx_sign;
 		if (dy_sign < 0) dy_sign = -dy_sign;
-		if (dx_sign < (int)Sign_width && dy_sign < (int)Sign_height)
+		if (dx_sign < (int)(Sign_width + Tree_width) && dy_sign < (int)(Sign_height + Tree_height))
 		{
 			collision = true;
 		}
